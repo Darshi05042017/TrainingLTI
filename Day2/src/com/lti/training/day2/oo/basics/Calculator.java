6e468bbff6c3ab0336afd84b5a1a01eea1e5fd71
vshadmin <vshadmin@Infva06687>
2019-06-01 16:43:24 -0700

    yes

---
+++ Day2/src/com/lti/training/day2/oo/basics/Calculator.java
@@ -0,0 +1,10 @@
+package com.lti.training.day2.oo.basics;
+
+public class Calculator {
+	public void add(int x,int y) {
+		System.out.println(x+y);
+	}
+	public void sub(int x,int y) {
+		System.out.println(x-y);
+	}
+}
