8d20ce844f59e77f9181ba31a52fd415e495f046
vshadmin <vshadmin@Infva06151>
2019-06-03 17:01:02 -0700

    updated

--- Day2/src/com/lti/BooksToys/App.java
+++ Day2/src/com/lti/BooksToys/App.java
@@ -4,7 +4,6 @@ public class App {
 	public static void main(String[] args) {
 		Book b1=new Book("Java", 99, 500.0, "A classic");
 		Book b2=new Book("Java Unleashed", 99, 400.0, "A classic");
-		b1=("Java", 99, 500.0, "A classic");
 		Toy t1=new Toy("RC car", 99, 250, "RC car", "4-5");
 		Toy t2=new Toy("Superhero", 99, 500, "Action", "5+");
 		
